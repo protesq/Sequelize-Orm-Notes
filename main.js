a8388f9eb572cc6554ba1aa307431b97d6ce6b5b
Arif <60351443+protesq@users.noreply.github.com>
2026-02-18 17:25:41 +0300

    Update main.js

--- main.js
+++ main.js
@@ -75,4 +75,89 @@ try {
     console.log('Connection closed.');
 }
 
+/* 
 
+import { Sequelize, DataTypes } from 'sequelize';
+
+const {DataTypes} = require('sequelize');
+
+Data Types:
+
+
+---String Types---
+DataTypes.STRING; // VARCHAR(255)
+DataTypes.TEXT; // VARCHAR(1234)
+DataTypes.STRING.BINARY; // VARCHAR BINARY
+DataTypes.TEXT; // TEXT
+DataTypes.TEXT('tiny'); // TINYTEXT
+DataTypes.CITEXT; // CITEXT POSTGRES ONLY
+DataTypes.TSVECTOR; // TSVECTOR POSTGRES ONLY
+
+---Boolean---
+DataTypes.BOOLEAN; // BOOLEAN
+
+---Numbers---
+DataTypes.INTEGER; // INTEGER
+DataTypes.BIGINT; // BIGINT
+DataTypes.BIGINT(11); // BIGINT(11)
+
+DataTypes.FLOAT; // FLOAT
+DataTypes.FLOAT(11); // FLOAT(11)
+DataTypes.FLOAT(11, 10); // FLOAT(11,10)
+
+DataTypes.REAL; // REAL            PostgreSQL only.
+DataTypes.REAL(11); // REAL(11)        PostgreSQL only.
+DataTypes.REAL(11, 12); // REAL(11,12)     PostgreSQL only.
+
+DataTypes.DOUBLE; // DOUBLE
+DataTypes.DOUBLE(11); // DOUBLE(11)
+DataTypes.DOUBLE(11, 10); // DOUBLE(11,10)
+
+DataTypes.DECIMAL; // DECIMAL
+DataTypes.DECIMAL(10, 2); // DECIMAL(10,2)
+
+*/
+
+//findOne - Veritabanından bir kayıt bulma
+try{
+    const user = await User_test.findOne({where: {id: 1}});
+    console.log('User found:', user.toJSON());
+} catch (error) {
+    console.error('Unable to find user:', error);
+} finally {
+    sequelize.close();
+    console.log('Connection closed.');
+}
+
+//create - Veritabanına yeni bir kayıt ekleme
+try{
+    const newUser = await User_test.create({username: 'testuser', password: 'testpassword', email: 'testuser@example.com', role: 'admin'});
+    console.log('User created:', newUser.toJSON());
+} catch (error) {
+    console.error('Unable to create user:', error);
+} finally {
+    sequelize.close();
+    console.log('Connection closed.');
+}
+
+// findByPk - Id üzerinden bir kayıt bulma
+try{
+    const user = await User_test.findByPk(1);
+    console.log('User found:', user.toJSON());
+} catch (error) {
+    console.error('Unable to find user:', error);
+} finally {
+    sequelize.close();
+    console.log('Connection closed.');
+}
+
+// update - Veritabanındaki bir kayıt güncelleme
+try{
+    const updatedUser = await User_test.update({username: 'testuser2'}, {where: {id: 1}});
+    console.log('User updated:', updatedUser.toJSON());
+} catch (error) {
+    console.error('Unable to update user:', error);
+} finally {
+    sequelize.close();
+    console.log('Connection closed.');
+}
